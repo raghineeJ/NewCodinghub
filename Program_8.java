269da8732d2547913edb610a74756498a96bf5bc
raghineeJ <103492808+raghineeJ@users.noreply.github.com>
2022-04-12 20:08:46 +0530

    Program_8.java
    This is a Task of TATHASTU SCHOLAR

---
+++ Program_8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Program_8
+ {
+   static boolean isMirrorInverse(int arr[])
+   {
+     for(int i=0;i<arr.length;i++)
+	 {
+	   if(arr[arr[i]!=i)
+	    return false;
+	 }
+	 return true;
+	 }
+	 public static void main(String args[])
+	  {
+	   int arr[]={1,2,3,0};
+	   if(is MirrorInverse(arr))
+	   System.out.println{"Yes");
+	   else
+	     System.out.println("No");
+		 }
+	}
+	   
+		
