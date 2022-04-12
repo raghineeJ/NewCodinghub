249c179348a970ec7a88d0e3bbf74f34b20834ea
raghineeJ <103492808+raghineeJ@users.noreply.github.com>
2022-04-12 20:07:06 +0530

    Program_7.java
    This is a task of TATHASTU SCHOLAR

---
+++ Program_7.java
@@ -0,0 +1,14 @@
+impo}rt java.util.Scanner;
+public class Program_7
+ {
+   public static void main(String args[])
+   String string="Dream big";
+    String reversedStr="";
+	for(int i=string.length()-1;i>=0;i--;
+	reversedStr=reversedStr+string.chsarAt(i);
+	{
+	  System.out.println("Original string:"+string);
+	  System.out.println("Reverse of given string:"+reversedStr);
+	  }
+	  }
+	
